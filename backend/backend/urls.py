73cac3f3c8b482b352e8cfc4fc20b7c2bbe4e5c1
aleena-techschool <aleena.techschool@gmail.com>
2026-01-16 11:01:15 +0530

    added repath

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,14 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path # re_path added TC
 from django.conf import settings
+from django.views.static import serve # added by TC
 from django.conf.urls.static import static
 
 
+
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path("",include("users.urls")),
+#     path("", include("events.urls")),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include("users.urls")),
     path("", include("events.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT}
+        ),]
