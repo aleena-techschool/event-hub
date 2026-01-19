3fbbb7a38439edbf7c433f2243abbb3ce7c2a156
aleena-techschool <aleena.techschool@gmail.com>
2026-01-19 16:50:27 +0530

    signup url fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,8 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
-DEBUG = os.environ.get("DEBUG") == "True"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret-key-123")
+# DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -129,6 +130,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 
 
