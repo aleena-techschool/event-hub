f2a5db323a40939ef7964ac7141bd8cf72e21ecf
aleena-techschool <aleena.techschool@gmail.com>
2026-01-19 17:35:50 +0530

    updated settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret-key-123")
-# DEBUG = os.environ.get("DEBUG") == "True"
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
+
 
 ALLOWED_HOSTS = ["*"]
 
